593058a5b0bf0930b4f8097c5399273e179a3601
Mykhailo-Mishyn <mykhailo_mishyn@epam.com>
2020-03-31 03:22:10 +0300

    Covered project with tests

---
+++ src/components/RegistrationForm/__tests__/index.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { FormMessage } from '../../../shared/components/Form/styled.js';
+import RegistrationForm from '../index.jsx';
+
+describe('<RegistrationForm />', () => {
+    const onClickMessageHandlerMock = jest.fn();
+    const mandatoryProps = {
+        onClickMessageHandler: onClickMessageHandlerMock,
+    };
+
+    it('should render correctly', () => {
+        const sut = mount(<RegistrationForm {...mandatoryProps} />);
+
+        expect(sut).toMatchSnapshot();
+    });
+
+    it('should call handler on click on redirection link', () => {
+        const sut = mount(<RegistrationForm {...mandatoryProps} />);
+
+        expect(onClickMessageHandlerMock).toBeCalledTimes(0);
+
+        sut.find(FormMessage).find('a').simulate('click');
+
+        expect(onClickMessageHandlerMock).toBeCalledTimes(1);
+    });
+});
